c22af2bed96a4222d25692647df034ee2315c7f5
Boris <borisfa88@gmail.com>
2025-03-26 15:19:18 +0200

    clean warnings

--- src/main/java/common/http/request/Application.java
+++ src/main/java/common/http/request/Application.java
@@ -2,10 +2,7 @@ package common.http.request;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
