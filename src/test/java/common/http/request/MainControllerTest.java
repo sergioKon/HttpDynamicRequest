e561584400ba674afeac387b15cb9b40e5e0373a
Boris <borisfa88@gmail.com>
2025-03-27 11:18:38 +0200

    Fix tests

--- src/test/java/common/http/request/MainControllerTest.java
+++ src/test/java/common/http/request/MainControllerTest.java
@@ -1,6 +1,5 @@
 package common.http.request;
 
-import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
